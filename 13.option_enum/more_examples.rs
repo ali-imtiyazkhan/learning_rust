e743c9841aacaaeee42af3d1d851973647500e8c
Imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-05-18 19:39:08 +0530

    fix: command fixed

--- 13.option_enum/more_examples.rs
+++ 13.option_enum/more_examples.rs
@@ -7,6 +7,7 @@ fn get_user_id(username: &str) -> Option<u32> {
 }
 
 fn main() {
+    // let user name is admin 
     let username = "admin";
     match get_user_id(username) {
         Some(id) => println!("User {} has ID: {}", username, id),
